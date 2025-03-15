31584754ab8946de7f2edb78eebd590139490916
ronaldandrade <andrade.ronald@gmail.com>
2025-03-15 02:15:29 -0300

    implementing filter by date&news

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,29 @@
-from flask import render_template, redirect, url_for
+import datetime
+from flask import render_template, redirect, url_for, request
 from .models import Noticia
 from .scraper import buscar_noticias
 from flask import current_app as app
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    data_filtro = request.args.get('data')
+    assunto_filtro = request.args.get('assunto')
+
+    query = Noticia.query
+
+    if data_filtro:
+        try:
+            data = datetime.strptime(data_filtro, '%Y-%m-%d')
+            query = query.filter(Noticia.data_publicacao >= data)
+        except ValueError:
+            pass
+
+        if assunto_filtro:
+            query = query.filter(
+                (Noticia.titulo.ilike(f'%{assunto_filtro}%')) |
+                (Noticia.conteudo.ilike(f'%{assunto_filtro}%'))
+            )
+        
     noticias = Noticia.query.order_by(Noticia.data_publicacao.desc()).all()
     print(f"Notícias carregadas do banco: {len(noticias)}")
     return render_template('index.html', noticias=noticias)
@@ -13,3 +32,8 @@ def index():
 def atualizar():
     buscar_noticias()
     return redirect(url_for('index'))
+
+@app.route("/noticia/<int:id>")
+def noticia(id):
+    noticia = Noticia.query.get_or_404(id)
+    return render_template('noticia.html', noticia=noticia)
