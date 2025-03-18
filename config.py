333b34e2253c18f2b9d9bb84169d54dfc3f96f40
ronaldandrade <andrade.ronald@gmail.com>
2025-03-18 01:01:48 -0300

    att DB

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///noticias.db'  # Banco SQLite simples
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///noticias.db' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
