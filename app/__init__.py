c9e5bbb1f7e4af30afba4c82be904082ab83c8e2
ronaldandrade <andrade.ronald@gmail.com>
2025-03-18 22:33:59 -0300

    run.py adjusts

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-    
-    db.init_app(app)
-    
-    with app.app_context():
-        from . import routes, models
-        db.create_all()
-    
-    return app
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    app.run()
